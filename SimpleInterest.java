7803e65659da1196c28e97f153fb7611f13d88af
Puneeth Raj Kumar M S <puneethms0707@gmail.com>
2026-01-30 07:59:08 +0530

    Create SimpleInterest.java

---
+++ SimpleInterest.java
@@ -0,0 +1,6 @@
+class SimpleInterest {
+    public static void main(String[] args) {
+        int p = 1000, r = 5, t = 2;
+        System.out.println("Simple Interest = " + (p * r * t) / 100);
+    }
+}
